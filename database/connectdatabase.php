515d75384835637bba1ac5c38321b4caca7e2913
fairyman22 <nguyenhoangviet2204@gmail.com>
2023-09-01 00:55:27 +0700

    databse

---
+++ database/connectdatabase.php
@@ -0,0 +1,15 @@
+<?php
+define('DATABASE_SERVER', 'localhost');
+define('DATABASE_USER', 'root');
+define('DATABASE_PASSWORD', '');
+define('DATABASE_NAME', 'webquanlyktx');
+$connection = null;
+try {
+    $connection = new PDO("mysql:host=" . DATABASE_SERVER . "dbname=" . DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD);
+    $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo "connected successfully";
+} catch (PDOException $th) {
+    //throw $th;
+}
+
+?>
